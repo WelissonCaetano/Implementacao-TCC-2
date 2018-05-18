255acafd992cb64b52003e7a0e84aaae2bd69dd8
Welisson Caetano da silva Leano <welisson.silva@sga.pucminas.br>
2018-05-18 03:47:06 -0300

    Commit do script R da implementação reinstruturado modularmnete juntamente com o professor

--- main.R
+++ main.R
@@ -1,47 +1,48 @@
 
 library(dplyr)
+library(ggplot2)
 
 #Rotinas auxiliares, como a de normalizacao
 source("C:/Users/caetano/Desktop/CognitiveProfiles/util.R")
 
 # Nome: calPropRespostasConcluisivasTrabalhador
-# Parâmetros: Todos: um data frame com a quantidade de resposta dada por cada um dos avaliadores
+# Par?metros: Todos: um data frame com a quantidade de resposta dada por cada um dos avaliadores
 #             Conclusivos: um data frame com a quantidade de respostas conclusivas dada por cada um dos avaliadores
-# Retorno: a Proporção de repostas Concluisivas por trabalhor em relação a proporção de respostas conclusivas no sistema 
+# Retorno: a Propor??o de repostas Concluisivas por trabalhor em rela??o a propor??o de respostas conclusivas no sistema 
 calPropRespConclTrabEmRelacaoPropResConclSistema <- function(Todos, Conclusivos) {
 
-  #Merge feito para Conclusivos e Resultado para a métrica Pwy.conclusivos
+  #Merge feito para Conclusivos e Resultado para a m?trica Pwy.conclusivos
   Conclusivos.Pwy <- merge(Todos, Conclusivos, by="rater")
   #Para cada trabalhador, divide a quantidade de respostas conclusivas dele pela quantidade total de respostas dele
   Conclusivos.Pwy$Tryw <- Conclusivos.Pwy$x.y/Conclusivos.Pwy$x.x
   
-  #Proporção de respostas conclusivas por trabalhador divido pala proporção de respostas conclusivas no sistema
+  #Propor??o de respostas conclusivas por trabalhador divido pala propor??o de respostas conclusivas no sistema
   Conclusivos.Pwy$pwy<-Conclusivos.Pwy$Tryw/(sum(Conclusivos$x)/length(base$judgment))
   
   Conclusivos.Pwy
 }
 # Nome: calPropRespInConclTrabEmRelacaoPropResInConclSistema
-# Parâmetros: Todos: um data frame com a quantidade de resposta dada por cada um dos avaliadores
+# Par?metros: Todos: um data frame com a quantidade de resposta dada por cada um dos avaliadores
 #             Inconclusivos: um data frame com a quantidade de respostas inconclusivas dada por cada um dos avaliadores
-# Retorno: a Proporção de repostas Inconclusivos por trabalhor em relação a proporção de respostas Inconclusivos no sistema 
+# Retorno: a Propor??o de repostas Inconclusivos por trabalhor em rela??o a propor??o de respostas Inconclusivos no sistema 
 calPropRespInConclTrabEmRelacaoPropResInConclSistema <- function(Todos, Inconclusivos) {
   
-    #Merge feito para Inconclusivos e Resultado para a métrica Pwi.inconclusivos
+    #Merge feito para Inconclusivos e Resultado para a m?trica Pwi.inconclusivos
     Pwy.inconclusivos <-merge(Todos,Inconclusivos,by ="rater")
     Pwy.inconclusivos$Tryw <- Pwy.inconclusivos[,3]/Pwy.inconclusivos[,2]
     Pwy.inconclusivos$pwi <- Pwy.inconclusivos$Tryw/(sum(Conclusivos$x)/length(base$judgment))
     Pwy.inconclusivos
 }
 # Nome: calPerRespPorTrabalhador
-# Parâmetros: Todos: um data frame com a quantidade de resposta dada por cada um dos avaliadores
+# Par?metros: Todos: um data frame com a quantidade de resposta dada por cada um dos avaliadores
 # Retorno: 0 percentual de repostas por trabalhor no sistema 
 calPerRespPorTrabalhador <- function (Todos) {
     Todos$Percentual <- (Todos$x / sum(Todos$x))*100
     Todos
 }
 # Nome: calDistVariaRespPorTrabEmRelVariaNoSistema
-# Parâmetros: base: um data frame orignal com os dados completos
-# Retorno: distancias: a distacia euclidiana entre o padrão de tipo de resposta do avaliador em relação a esse mesmo padrão com os tipo de resposta no sistema 
+# Par?metros: base: um data frame orignal com os dados completos
+# Retorno: distancias: a distacia euclidiana entre o padr?o de tipo de resposta do avaliador em rela??o a esse mesmo padr?o com os tipo de resposta no sistema 
 
 calDistVariaRespPorTrabEmRelVariaNoSistema <- function (base){
   
@@ -53,7 +54,7 @@ calDistVariaRespPorTrabEmRelVariaNoSistema <- function (base){
                              QTD_4 = c(),
                              stringsAsFactors = FALSE)
   nrow(base) 
-  # Começa o loop
+  # Come?a o loop
   
   while(nrow(base) > 0){
     #converte a coluna rater par string
@@ -66,7 +67,7 @@ calDistVariaRespPorTrabEmRelVariaNoSistema <- function (base){
     jugamentoVetor <- as.vector(jugamentos$`baseUno$judgment`)
     #converte o vetor em um dataframe
     jugamentoData <- data.frame(judgment=c(jugamentoVetor))
-    #Realiza a contagem de repetiçoes para cada jugamento do rater e quarda em quadroQTD
+    #Realiza a contagem de repeti?oes para cada jugamento do rater e quarda em quadroQTD
     quadroQTD <- jugamentoData %>%
       group_by(judgment) %>%
       summarise(repetido = n()) %>%
@@ -88,10 +89,10 @@ calDistVariaRespPorTrabEmRelVariaNoSistema <- function (base){
     baseDerivada <- rbind.data.frame(baseDerivada, RaterDerivada)
     #Deleta do ambiente a dataframe RaterDerivada
     remove(RaterDerivada)
-    #Exclui totalemte o rater da que foi derivado da base inicia(Isso se repete até a base ter zero rows)
+    #Exclui totalemte o rater da que foi derivado da base inicia(Isso se repete at? a base ter zero rows)
     base <- as.data.frame(subset(base, rater != base[1,2]))
   }
-  #Para as colunas em que nao houve repetição o sistema atribui NA nesse comando substituimos pelo o valor Zero
+  #Para as colunas em que nao houve repeti??o o sistema atribui NA nesse comando substituimos pelo o valor Zero
   baseDerivada[is.na(baseDerivada)] <- 0
   
   Szero = sum(baseDerivada$QTD_0)
@@ -129,23 +130,27 @@ calDistVariaRespPorTrabEmRelVariaNoSistema <- function (base){
   distancias
 }
 
-analiseDoMelhorK(Metricas)
 
 analiseDoMelhorK <- function(Metricas){
   
-  #Verificando o número ideal de clusters dado os dados com o Método Elbow  
+  #Verificando o n?mero ideal de clusters dado os dados com o M?todo Elbow  
   wss <- (nrow(Metricas)-1)*sum(apply(Metricas,2,var))
   for (i in 2:15) wss[i] <- sum(kmeans(Metricas,
                                        centers=i)$withinss)
+  
+  png("ErroPorNumeroDeGrupos.png")
   plot(1:15, wss, type="b", xlab="Numero de Clusters",
        ylab="Soma dos quadrados dentro dos grupos ",
        main="",
        pch=20, cex=2)
+  dev.off()
   
 }
 
+
+
 # Carregamento da base de dados 
-base <- as.data.frame(read.csv("C:\Users\caetano\Desktop\cf-sentiment-basic.csv"))
+base <- as.data.frame(read.csv("C:/Users/caetano/Desktop/cf-sentiment-basic.csv"))
 
 #Seleciona respostas inconclusivas e conta a quantidade de repostas inconclusivas por avaliador
 baseI <- as.data.frame(subset(base,judgment == 4))
@@ -156,99 +161,112 @@ baseC <- as.data.frame(subset(base,judgment != 4))
 Conclusivos <-aggregate(baseC$rater,by = list(rater = baseC$rater),length)
 
 #conta a quantidade de repostas por avaliador
-Todos <-aggregate(base$rater,by = list(rater = base$rater),length)
+Todos <- aggregate(base$rater,by = list(rater = base$rater),length)
 
-Pwy.conclusivos <- calPropRespConclTrabEmRelacaoPropResConclSistema(Todos, Conclusivos)
 
-Pwy.inconclusivos <- calPropRespInConclTrabEmRelacaoPropResInConclSistema(Todos,Inconclusivos)
+
+#Calcula metricas 
 
 Pwy.todos <- calPerRespPorTrabalhador(Todos)
-
-distancias <- calDistVariaRespPorTrabEmRelVariaNoSistema(base)
-
-# cria um subset com a metrica e os rater de Pwy.conclusivos
-MetricaPwyConclusivos <- subset(Pwy.conclusivos, select = c("rater","pwy"))
-min(MetricaPwyConclusivos$pwy)
-max(MetricaPwyConclusivos$pwy)
-MetricaPwyConclusivosNormalizada <- Normaliza(MetricaPwyConclusivos)
-min(MetricaPwyConclusivosNormalizada$pwy)
-max(MetricaPwyConclusivosNormalizada$pwy)
-
-# cria um subset com a metrica e os rater de Pwy.Inconclusivos
-MetricaPwyInconclusivos <- subset(Pwy.inconclusivos, select = c("rater","pwi"))
-min(MetricaPwyInconclusivos$pwi)
-max(MetricaPwyInconclusivos$pwi)
-MetricaPwyInconclusivosNormalizada <- Normaliza(MetricaPwyInconclusivos)
-min(MetricaPwyInconclusivosNormalizada$pwi)
-max(MetricaPwyInconclusivosNormalizada$pwi)
-
 # cria um subset com a metrica e os rater de Pwy.Todos
 MetricaPwycTodos <- subset(Pwy.todos, select = c("rater","Percentual"))
-min(MetricaPwycTodos$Percentual)
-max(MetricaPwycTodos$Percentual)
 MetricaPwycTodosNormalizada <- Normaliza(MetricaPwycTodos)
-min(MetricaPwycTodosNormalizada$Percentual)
-max(MetricaPwycTodosNormalizada$Percentual)
 
-# cria um dataframe que reuni as colunas em uma so tabela de nome 'ColunaDeMétricas'.
-ColunaDeMétricas <- join(MetricaPwycTodosNormalizada, MetricaPwyConclusivosNormalizada, by="rater", type="left")
-ColunaDeMétricas <- join(ColunaDeMétricas, MetricaPwyInconclusivosNormalizada, by="rater", type="left")
-nrow(ColunaDeMétricas)
+Pwy.conclusivos <- calPropRespConclTrabEmRelacaoPropResConclSistema(Todos, Conclusivos)
+# cria um subset com a metrica e os rater de Pwy.conclusivos
+MetricaPwyConclusivos <- subset(Pwy.conclusivos, select = c("rater","pwy"))
+MetricaPwyConclusivosNormalizada <- Normaliza(MetricaPwyConclusivos)
 
-#Atribui 0 as colunas que possui linha com valor 'NA'
-ColunaDeMétricas[is.na(ColunaDeMétricas)] <- 0
+Pwy.inconclusivos <- calPropRespInConclTrabEmRelacaoPropResInConclSistema(Todos,Inconclusivos)
+# cria um subset com a metrica e os rater de Pwy.Inconclusivos
+MetricaPwyInconclusivos <- subset(Pwy.inconclusivos, select = c("rater","pwi"))
+MetricaPwyInconclusivosNormalizada <- Normaliza(MetricaPwyInconclusivos)
 
-#renomeia as colunas da tabela com os nomes das métricas
-names(ColunaDeMétricas) = c("raters","Pwc", "Pwi", "Pw")
-
-
+distancias <- calDistVariaRespPorTrabEmRelVariaNoSistema(base)
 #Renomeia a coluna trabalhadores par raters
-names(distancias) = c("raters", "distancia")
-min(distancias$distancia)
-max(distancias$distancia)
+names(distancias) = c("rater", "distancia")
 distanciasNormalizada <- Normaliza(distancias)
-min(distanciasNormalizada$distancia)
-max(distanciasNormalizada$distancia)
 
-#Realiao o join da tabela de metrica com a nova metrica que foi derivada
-ColunaDeMétricasCompleta <- join(ColunaDeMétricas,distanciasNormalizada, by="raters", type="left")
 
-#renomeia as colunas da tabela com os nomes das métricas
-names(ColunaDeMétricasCompleta) = c("raters","Pwc", "Pwi", "Pw", "DEw")
+df.metrics.naoNormalizadas <- AgregaDFByRater(MetricaPwycTodos,MetricaPwyConclusivos,MetricaPwyInconclusivos,distancias)
+print(head(ColunaDeMetricasCompleta))
+names(df.metrics.naoNormalizadas) = c("rater","Pwc", "Pwi", "Pw", "DEw")
 
-#Dataframe com puramente dados numericos sem a lista de raters
-Metricas <- data.frame(ColunaDeMétricasCompleta[,2:5])
+ColunaDeMetricasCompleta <- AgregaDFByRater(MetricaPwycTodosNormalizada,MetricaPwyConclusivosNormalizada,MetricaPwyInconclusivosNormalizada,distanciasNormalizada)
+print(head(ColunaDeMetricasCompleta))
+names(ColunaDeMetricasCompleta) = c("rater","Pwc", "Pwi", "Pw", "DEw")
 
-boxplot(Metricas, las=1)
+#Atribui 0 no valor da metrica de algum rater que nÃ£o valor naquela metrica
+#IMPORTANTE:lesandrop(comentei essa linha para ver efeitos nos resultados e checar a necessidade dela)
+ColunaDeMetricasCompleta[is.na(ColunaDeMetricasCompleta)] <- 0
+df.metrics.naoNormalizadas[is.na(df.metrics.naoNormalizadas)] <- 0
+
+df.metrics.naoNormalizadas$Pw <- NULL #seja apenas as tres metricas relevantes
+head(df.metrics.naoNormalizadas)
+
+png("boxPlotMetricasNaoNormalizadas.png")
+boxplot(df.metrics.naoNormalizadas[,2:4], las=1)
+dev.off()
+
+
+ColunaDeMetricasCompleta$Pw <- NULL #seja apenas as tres metricas relevantes
+head(ColunaDeMetricasCompleta)
+
+png("boxPlotMetricasNormalizadas.png")
+boxplot(ColunaDeMetricasCompleta[,2:4], las=1)
+dev.off()
+
+##
+##Analise do Agrapamentos
+##
 
 set.seed(7)
 
+#Dataframe com puramente dados numericos sem a lista de raters
+Metricas <- data.frame(ColunaDeMetricasCompleta[,2:4])
+head(Metricas)
+
+
+##
+##Analise grafica do melhor k
+##
 analiseDoMelhorK(Metricas)
 
 
-#Execute K-Means com o número ótimo de clusters identificados a partir do método Elbow
 
-Resultado = kmeans(Metricas, 5, nstart=100)
-Resultado$centers
+##
+##Execucao do k-means para o melhor k
+##
+Resultado <- kmeans(Metricas, 4, nstart=100)
 
-#
-plot(Metricas, col =(Resultado$cluster +1) , main="K-Means resultados com 5 
+
+##
+##Exibicao dos centroids resultantes do k-means
+##
+plot(Metricas, col =(Resultado$cluster +1) , main="K-Means resultados com 4 
      clusters", pch=20, cex=2)
 
-a <- seq(1:5)
+a <- seq(1:4)
 
 ndf <- as.data.frame(Resultado$centers)
 
 ndf$Centroids <- a
 
-MetricasVertival <- as.data.frame(rbind(cbind("Centroid"=ndf$Centroids,"Valor"=ndf$Pwc,"Metrica"=rep("Pwc",5)),
-                                        cbind("Centroid"=ndf$Centroids,"Valor"=ndf$Pwi,"Metrica"=rep("Pwi",5)),
-                                        cbind("Centroid"=ndf$Centroids,"Valor"=ndf$Pw,"Metrica"=rep("Pw",5)),
-                                        cbind("Centroid"=ndf$Centroids,"Valor"=ndf$DEw,"Metrica"=rep("DEw",5))))
+MetricasVertival <- as.data.frame(rbind(cbind("Centroid"=ndf$Centroids,"Valor"=ndf$Pwc,"Metrica"=rep("Pwc",4)),
+                                        cbind("Centroid"=ndf$Centroids,"Valor"=ndf$Pwi,"Metrica"=rep("Pwi",4)),
+  #                                      cbind("Centroid"=ndf$Centroids,"Valor"=ndf$Pw,"Metrica"=rep("Pw",5)),
+                                        cbind("Centroid"=ndf$Centroids,"Valor"=ndf$DEw,"Metrica"=rep("DEw",4))))
 
+
+p <- ggplot(data=MetricasVertival, aes(x=Centroid, y=Valor, fill=Metrica)) + 
+  geom_bar(stat="identity", position="dodge")
+
+png("centroids.png")
+print(p)
+dev.off()
 
 # Grouped barplot
-barplot(Resultado$centers, col=colors()[c(617,23,89,12,148)] , border="white", font.axis=2, beside=T, xlab="group", font.lab=2)
+#barplot(Resultado$centers, col=colors()[c(617,23,89,12,148)] , border="white", font.axis=2, beside=T, xlab="group", font.lab=2)
 
 ## Grouped barplot
 #barplot(MetricasVertival, col=colors()[c(617,23,89,12,148)] , border="white", font.axis=2, beside=T, xlab="group", font.lab=2)
--- util.R
+++ util.R
@@ -1,7 +1,17 @@
-#Função para normalizar as metrica com valores entre 0 e 1
-Normaliza = function (dados) {
-  maior = max(dados[,2])
-  menor = min(dados[,2])
-  dados[,2] = (dados[,2] - menor) / (maior - menor)
+#Fun??o para normalizar as metrica com valores entre 0 e 1
+
+Normaliza <- function (dados) {
+  maior <- max(dados[,2])
+  menor <- min(dados[,2])
+  dados[,2] <- (dados[,2] - menor) / (maior - menor)
   return (dados)
+}
+
+AgregaDFByRater <- function(Df1, Df2, Df3, df4){
+ 
+  df.joined <- join(Df1, Df2, by="rater", type="left")
+  df.joined <- join(df.joined, Df3, by="rater", type="left")
+  df.joined <- join(df.joined,df4, by="rater", type="left")
+  df.joined
+  
 }
